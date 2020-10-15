bf1f6a861cb48ab4caacbc6baa1020c6bb431768
Kamil Kowalewski <szamil24@gmail.com>
2020-10-15 16:04:00 +0200

    Add FxHelper.java + small fix

--- src/main/java/pl/awjkmkkk/view/constant/ViewConstants.java
+++ src/main/java/pl/awjkmkkk/view/constant/ViewConstants.java
@@ -4,7 +4,7 @@ public class ViewConstants {
 
     /*------------------------ FIELDS REGION ------------------------*/
     public static final String PATH_MAIN_PANEL = "/panel/MainPanel.fxml";
-    public static final String TITLE_MAIN_PANEL = "pl.awjkmkkk.Main Panel";
+    public static final String TITLE_MAIN_PANEL = "Main Panel";
     public static final Integer MAIN_PANEL_WIDTH = 1200;
     public static final Integer MAIN_PANEL_HEIGHT = 700;
     public static final String PATH_CSS_DARK_STYLING = "/style/DarkStyles.css";
