9a2451e1bbf1173dbc9546a880e349c6649321ef
Kamil Kowalewski <szamil24@gmail.com>
2020-10-28 11:27:07 +0100

    Add commandLineRunner in Main class

--- src/main/java/pl/teamsix/competenceproject/Main.java
+++ src/main/java/pl/teamsix/competenceproject/Main.java
@@ -1,13 +1,24 @@
 package pl.teamsix.competenceproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    /**
+     * This is the real Spring boot main method where fg. UI can be started
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        // TODO DELETE BELOW CODE, THIS IS ONLY FOR SHOW PURPOSES
+        System.out.println("---------------------------------");
+        System.out.println("SAMPLE TEXT TO DELETE");
+        System.out.println("---------------------------------");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
 }
