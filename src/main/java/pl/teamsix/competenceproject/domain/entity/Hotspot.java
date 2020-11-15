cecbda356e2016b0547a5bffe68571d6df307c4a
Laureldil2 <d.szczepanski97@gmail.com>
2020-11-15 19:00:30 +0100

    Add point generation

--- src/main/java/pl/teamsix/competenceproject/domain/entity/Hotspot.java
+++ src/main/java/pl/teamsix/competenceproject/domain/entity/Hotspot.java
@@ -7,6 +7,22 @@ public class Hotspot extends BaseEntity {
     private String name;
     private String description;
     private String type;
-    private float x;
-    private float y;
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
--- src/main/java/pl/teamsix/competenceproject/logic/generation/HotspotsGenerator.java
+++ src/main/java/pl/teamsix/competenceproject/logic/generation/HotspotsGenerator.java
@@ -1,26 +1,43 @@
 package pl.teamsix.competenceproject.logic.generation;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
+import org.apache.commons.rng.RestorableUniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
+import org.apache.commons.statistics.distribution.ContinuousDistribution;
+import org.apache.commons.statistics.distribution.GammaDistribution;
 import org.springframework.stereotype.Service;
 import pl.teamsix.competenceproject.domain.entity.Hotspot;
 import pl.teamsix.competenceproject.domain.service.hotspot.HotspotService;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class HotspotsGenerator {
 
     private static final int BATCH_SIZE = 100;
-
-    private final HotspotService hotspotService;
+    private double maxDistance;
+    private ContinuousDistribution.Sampler sampler;
+    private final RestorableUniformRandomProvider rand;
+    private  HotspotService hotspotService;
 
     public HotspotsGenerator(final HotspotService hotspotService) {
+        this.rand = RandomSource.create(RandomSource.MT);
+        this.sampler = new GammaDistribution(1,2).createSampler(rand);
+        this.maxDistance = 5000;
         this.hotspotService = hotspotService;
+    }
+
+    public void setMaxDistance(double maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+
+    public void setGammaParams(double shape, double scale) {
+        this.sampler = new GammaDistribution(shape,scale).createSampler(rand);
     }
 
     public void generate(int quantity) {
         for (int i = 0; i < quantity; i += BATCH_SIZE) {
-            hotspotService.saveAll(IntStream.range(0, Math.min(quantity - i, BATCH_SIZE))
+           hotspotService.saveAll(IntStream.range(0, Math.min(quantity - i, BATCH_SIZE))
                     .mapToObj(x -> generateSingleHotspot())
                     .collect(Collectors.toList()));
         }
@@ -34,6 +51,12 @@ public class HotspotsGenerator {
     }
 
     private void generateHotspotPosition(Hotspot hotspot) {
-        //TODO Dominik - fill x, y values
+        double  angle = rand.nextDouble()*2*Math.PI; //uniform distribution
+        double distance; //gamma distribution
+        do{
+            distance = sampler.sample()*1000;}
+        while(distance>this.maxDistance);
+        hotspot.setX(distance*Math.cos(angle));
+        hotspot.setY(distance*Math.sin(angle));
     }
 }
