d3c595dc3cb0d0276a9199a53324c83bcea7c9ef
Kamil Kowalewski <szamil24@gmail.com>
2020-12-03 19:25:22 +0100

    Fix missed inheritance

--- src/main/java/pl/teamsix/competenceproject/domain/entity/UserBackup.java
+++ src/main/java/pl/teamsix/competenceproject/domain/entity/UserBackup.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.ArrayList;
 
 @Document
-public class UserBackup {
+public class UserBackup extends BaseEntity {
 
     /*------------------------ FIELDS REGION ------------------------*/
     private String lastName;
