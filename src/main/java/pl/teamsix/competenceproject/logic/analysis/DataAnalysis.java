058a8c208e3cf1239884609d23e98ba00d5be33c
Skyblade <piotr8715@gmail.com>
2020-12-11 00:12:31 +0100

    Added DataAnalysis.numberOfUsersByHours which displays number of users which were present in given hotspot in given time frame

--- src/main/java/pl/teamsix/competenceproject/logic/analysis/DataAnalysis.java
+++ src/main/java/pl/teamsix/competenceproject/logic/analysis/DataAnalysis.java
@@ -109,4 +109,20 @@ public class DataAnalysis {
         System.out.println("userTimeSpentInHotspot");
     }
 
+    public void numberOfUsersByHours(){
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        Dataset<Row> df = MongoSpark.load(jsc).toDF();
+        Dataset<Row> temp =  df
+            .select(
+                col("hotspot"),
+                when(hour(col("entryTime")).between(0, 2), 1).when(hour(col("exitTime")).between(0, 2), 1).otherwise(0).as("entered0"), when(hour(col("entryTime")).between(2, 4), 1).when(hour(col("exitTime")).between(2, 4), 1).otherwise(0).as("entered1"), when(hour(col("entryTime")).between(4, 6), 1).when(hour(col("exitTime")).between(4, 6), 1).otherwise(0).as("entered2"), when(hour(col("entryTime")).between(6, 8), 1).when(hour(col("exitTime")).between(6, 8), 1).otherwise(0).as("entered3"), when(hour(col("entryTime")).between(8, 10), 1).when(hour(col("exitTime")).between(8, 10), 1).otherwise(0).as("entered4"), when(hour(col("entryTime")).between(10, 12), 1).when(hour(col("exitTime")).between(10, 12), 1).otherwise(0).as("entered5"), when(hour(col("entryTime")).between(12, 14), 1).when(hour(col("exitTime")).between(12, 14), 1).otherwise(0).as("entered6"), when(hour(col("entryTime")).between(14, 16), 1).when(hour(col("exitTime")).between(14, 16), 1).otherwise(0).as("entered7"), when(hour(col("entryTime")).between(16, 18), 1).when(hour(col("exitTime")).between(16, 18), 1).otherwise(0).as("entered8"), when(hour(col("entryTime")).between(18, 20), 1).when(hour(col("exitTime")).between(18, 20), 1).otherwise(0).as("entered9"), when(hour(col("entryTime")).between(20, 22), 1).when(hour(col("exitTime")).between(20, 22), 1).otherwise(0).as("entered10"), when(hour(col("entryTime")).between(22, 24), 1).when(hour(col("exitTime")).between(22, 24), 1).otherwise(0).as("entered11")
+            )
+            .groupBy("hotspot")
+            .agg(
+                    sum("entered0").as("People in between 0-2"), sum("entered1").as("People in between 2-4"), sum("entered2").as("People in between 4-6"), sum("entered3").as("People in between 6-8"), sum("entered4").as("People in between 8-10"), sum("entered5").as("People in between 10-12"), sum("entered6").as("People in between 12-14"), sum("entered7").as("People in between 14-16"), sum("entered8").as("People in between 16-18"), sum("entered9").as("People in between 18-20"), sum("entered10").as("People in between 20-22"), sum("entered11").as("People in between 22-24")
+            );
+
+        temp.show(200,false);
+        System.out.println("numberOfUsersByHours");
+    }
 }
