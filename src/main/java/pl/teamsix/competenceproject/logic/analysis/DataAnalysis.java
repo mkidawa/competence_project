dde2563942347eabd9bdfab6b24f2a7abbd96526
Skyblade <piotr8715@gmail.com>
2020-12-15 17:06:47 +0100

    Added method DataAnalysis.longestRoute which shows longest Route took by each user(very long computing time - it is advised to use .limit(int) method, when selecting tempUser, so method will calculate results for less users. Added slight optimization methods and changed formatting in DataAnalysis

--- src/main/java/pl/teamsix/competenceproject/logic/analysis/DataAnalysis.java
+++ src/main/java/pl/teamsix/competenceproject/logic/analysis/DataAnalysis.java
@@ -1,13 +1,20 @@
 package pl.teamsix.competenceproject.logic.analysis;
 
 import com.mongodb.spark.MongoSpark;
-import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import com.mongodb.spark.config.ReadConfig;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.scheduler.IndirectTaskResult;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.bson.Document;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -39,15 +46,15 @@ root
  */
 @Service
 public class DataAnalysis {
-    private final static SparkSession spark = SparkSession.builder()
+    private SparkSession sparkTrace = SparkSession.builder()
             .master("local")
             .appName("appname")
             .config("spark.mongodb.input.uri", "mongodb://localhost:27017/competence_project_name.trace")
             .config("spark.mongodb.output.uri", "mongodb://localhost:27017/competence_project_name.trace")
             .getOrCreate();
 
-    public void rankByUsersInHotspot() {
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+    public void rankByUsersInHotspot(){
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
         Dataset<Row> df = MongoSpark.load(jsc).toDF();
         Dataset<Row> result = df
                 .groupBy("hotspot")
@@ -61,22 +68,38 @@ public class DataAnalysis {
     }
 
     public void rankByTimeSpentInHotspot() {
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
         Dataset<Row> df = MongoSpark.load(jsc).toDF();
-        Dataset<Row> xd = df.select(col("hotspot"), col("exitTime").cast("long").minus(col("entryTime").cast("long")).as("xd"), to_timestamp(col("exitTime")).minus(to_timestamp(col("entryTime"))).as("timeSpent"));
-        xd.sort(col("xd").desc()).show(30);
-        //xd.show(200);
+        Dataset<Row> xd =  df.select(
+                col("hotspot"),
+                col("exitTime").cast("long")
+                        .minus(col("entryTime").cast("long")).as("xd"),
+                to_timestamp(col("exitTime"))
+                        .minus(to_timestamp(col("entryTime"))).as("timeSpent")
+                ).sort(col("xd").desc()
+        );
+        xd.show(30);
         System.out.println(xd.count());
-        Dataset<Row> times = df.select(col("hotspot"), col("exitTime").cast("long").minus(col("entryTime").cast("long")).divide(60).as("timeSpent"));
+        Dataset<Row> times = df.select(
+                col("hotspot"),
+                col("exitTime").cast("long")
+                    .minus(col("entryTime").cast("long")).divide(60).as("timeSpent")
+            );
         //Dataset<Row> result = times.groupBy("hotspot").agg(sum(col("timeSpent")),avg(col("timeSpent")),max(col("timeSpent")));
-        Dataset<Row> result = times.groupBy("hotspot").agg(round(sum(col("timeSpent")), 2), round(avg(col("timeSpent")), 2), round(max(col("timeSpent")), 2));
+        Dataset<Row> result = times
+                .groupBy("hotspot")
+                .agg(
+                        round(sum(col("timeSpent")), 2),
+                        round(avg(col("timeSpent")), 2),
+                        round(max(col("timeSpent")), 2)
+                );
         result.show(false);
         jsc.close();
         System.out.println("rankByTimeSpentInHotspot");
     }
 
     public void rankByFrequentUsers() {
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
         Dataset<Row> df = MongoSpark.load(jsc).toDF();
         Dataset<Row> frequency = df
                 .groupBy("hotspot", "user")
@@ -84,7 +107,9 @@ public class DataAnalysis {
                 .sort(col("count")
                         .desc()
                 )
-                .groupBy("hotspot").agg(first("user"), max("count").as("maximum"))
+                .groupBy("hotspot").agg(
+                        first("user"),
+                        max("count").as("maximum"))
                 .sort(
                         col("maximum").desc()
                 );
@@ -93,8 +118,8 @@ public class DataAnalysis {
         System.out.println("rankByFrequentUsers");
     }
 
-    public void userTimeSpentInHotspot() {
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+    public void userTimeSpentInHotspot(){
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
         Dataset<Row> df = MongoSpark.load(jsc).toDF();
         Dataset<Row> times = df
                 .select(
@@ -116,23 +141,61 @@ public class DataAnalysis {
     }
 
     public void numberOfUsersByHours() {
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
-        Dataset<Row> df = MongoSpark.load(jsc).toDF();
-        Dataset<Row> temp = df
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
+        Dataset<Row> temp = MongoSpark.load(jsc).toDF()
                 .select(
                         col("hotspot"),
-                        when(
-                                hour(col("entryTime"))
-                                        .between(0, 2), 1)
-                                .when(
-                                        hour(col("exitTime"))
-                                                .between(0, 2), 1)
+                        when(hour(col("entryTime")).between(0, 2), 1)
+                                .when(hour(col("exitTime")).between(0, 2), 1)
                                 .otherwise(0).as("entered0"),
-                        when(hour(col("entryTime")).between(2, 4), 1).when(hour(col("exitTime")).between(2, 4), 1).otherwise(0).as("entered1"), when(hour(col("entryTime")).between(4, 6), 1).when(hour(col("exitTime")).between(4, 6), 1).otherwise(0).as("entered2"), when(hour(col("entryTime")).between(6, 8), 1).when(hour(col("exitTime")).between(6, 8), 1).otherwise(0).as("entered3"), when(hour(col("entryTime")).between(8, 10), 1).when(hour(col("exitTime")).between(8, 10), 1).otherwise(0).as("entered4"), when(hour(col("entryTime")).between(10, 12), 1).when(hour(col("exitTime")).between(10, 12), 1).otherwise(0).as("entered5"), when(hour(col("entryTime")).between(12, 14), 1).when(hour(col("exitTime")).between(12, 14), 1).otherwise(0).as("entered6"), when(hour(col("entryTime")).between(14, 16), 1).when(hour(col("exitTime")).between(14, 16), 1).otherwise(0).as("entered7"), when(hour(col("entryTime")).between(16, 18), 1).when(hour(col("exitTime")).between(16, 18), 1).otherwise(0).as("entered8"), when(hour(col("entryTime")).between(18, 20), 1).when(hour(col("exitTime")).between(18, 20), 1).otherwise(0).as("entered9"), when(hour(col("entryTime")).between(20, 22), 1).when(hour(col("exitTime")).between(20, 22), 1).otherwise(0).as("entered10"), when(hour(col("entryTime")).between(22, 24), 1).when(hour(col("exitTime")).between(22, 24), 1).otherwise(0).as("entered11")
+                        when(hour(col("entryTime")).between(2, 4), 1)
+                                .when(hour(col("exitTime")).between(2, 4), 1)
+                                .otherwise(0).as("entered1"),
+                        when(hour(col("entryTime")).between(4, 6), 1)
+                                .when(hour(col("exitTime")).between(4, 6), 1)
+                                .otherwise(0).as("entered2"),
+                        when(hour(col("entryTime")).between(6, 8), 1)
+                                .when(hour(col("exitTime")).between(6, 8), 1)
+                                .otherwise(0).as("entered3"),
+                        when(hour(col("entryTime")).between(8, 10), 1)
+                                .when(hour(col("exitTime")).between(8, 10), 1)
+                                .otherwise(0).as("entered4"),
+                        when(hour(col("entryTime")).between(10, 12), 1)
+                                .when(hour(col("exitTime")).between(10, 12), 1)
+                                .otherwise(0).as("entered5"),
+                        when(hour(col("entryTime")).between(12, 14), 1)
+                                .when(hour(col("exitTime")).between(12, 14), 1)
+                                .otherwise(0).as("entered6"),
+                        when(hour(col("entryTime")).between(14, 16), 1)
+                                .when(hour(col("exitTime")).between(14, 16), 1)
+                                .otherwise(0).as("entered7"),
+                        when(hour(col("entryTime")).between(16, 18), 1)
+                                .when(hour(col("exitTime")).between(16, 18), 1)
+                                .otherwise(0).as("entered8"),
+                        when(hour(col("entryTime")).between(18, 20), 1)
+                                .when(hour(col("exitTime")).between(18, 20), 1)
+                                .otherwise(0).as("entered9"),
+                        when(hour(col("entryTime")).between(20, 22), 1)
+                                .when(hour(col("exitTime")).between(20, 22), 1)
+                                .otherwise(0).as("entered10"),
+                        when(hour(col("entryTime")).between(22, 24), 1)
+                                .when(hour(col("exitTime")).between(22, 24), 1)
+                                .otherwise(0).as("entered11")
                 )
                 .groupBy("hotspot")
                 .agg(
-                        sum("entered0").as("People in between 0-2"), sum("entered1").as("People in between 2-4"), sum("entered2").as("People in between 4-6"), sum("entered3").as("People in between 6-8"), sum("entered4").as("People in between 8-10"), sum("entered5").as("People in between 10-12"), sum("entered6").as("People in between 12-14"), sum("entered7").as("People in between 14-16"), sum("entered8").as("People in between 16-18"), sum("entered9").as("People in between 18-20"), sum("entered10").as("People in between 20-22"), sum("entered11").as("People in between 22-24")
+                        sum("entered0").as("People in between 0-2"),
+                        sum("entered1").as("People in between 2-4"),
+                        sum("entered2").as("People in between 4-6"),
+                        sum("entered3").as("People in between 6-8"),
+                        sum("entered4").as("People in between 8-10"),
+                        sum("entered5").as("People in between 10-12"),
+                        sum("entered6").as("People in between 12-14"),
+                        sum("entered7").as("People in between 14-16"),
+                        sum("entered8").as("People in between 16-18"),
+                        sum("entered9").as("People in between 18-20"),
+                        sum("entered10").as("People in between 20-22"),
+                        sum("entered11").as("People in between 22-24")
                 );
 
         temp.show(200, false);
@@ -141,20 +204,19 @@ public class DataAnalysis {
     }
 
     public void numberOfUsersByWeekDay(){
-        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
-        Dataset<Row> df = MongoSpark.load(jsc).toDF();
-        Dataset<Row> temp =  df
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
+        Dataset<Row> temp = MongoSpark.load(jsc).toDF()
                 .select(
-                        col("hotspot")
-                        ,when(dayofweek(col("entryTime")).equalTo(1),1).otherwise(0).as("entered1")
-                        ,when(dayofweek(col("entryTime")).equalTo(2),1).otherwise(0).as("entered2")
-                        ,when(dayofweek(col("entryTime")).equalTo(3),1).otherwise(0).as("entered3")
-                        ,when(dayofweek(col("entryTime")).equalTo(4),1).otherwise(0).as("entered4")
-                        ,when(dayofweek(col("entryTime")).equalTo(5),1).otherwise(0).as("entered5")
-                        ,when(dayofweek(col("entryTime")).equalTo(6),1).otherwise(0).as("entered6")
-                        ,when(dayofweek(col("entryTime")).equalTo(7),1).otherwise(0).as("entered7")
+                col("hotspot")
+                ,when(dayofweek(col("entryTime")).equalTo(1),1).otherwise(0).as("entered1")
+                ,when(dayofweek(col("entryTime")).equalTo(2),1).otherwise(0).as("entered2")
+                ,when(dayofweek(col("entryTime")).equalTo(3),1).otherwise(0).as("entered3")
+                ,when(dayofweek(col("entryTime")).equalTo(4),1).otherwise(0).as("entered4")
+                ,when(dayofweek(col("entryTime")).equalTo(5),1).otherwise(0).as("entered5")
+                ,when(dayofweek(col("entryTime")).equalTo(6),1).otherwise(0).as("entered6")
+                ,when(dayofweek(col("entryTime")).equalTo(7),1).otherwise(0).as("entered7")
 
-                )
+        )
                 .groupBy("hotspot")
                 .agg(
                         sum("entered2").as("People in Monday"),
@@ -164,9 +226,90 @@ public class DataAnalysis {
                         sum("entered6").as("People in Friday"),
                         sum("entered7").as("People in Saturday"),
                         sum("entered1").as("People in Sunday")
-                        );
+                );
 
         temp.show(200,false);
         System.out.println("numberOfUsersByWeekDay");
+    }
+
+    public void longestRoute(){
+
+        JavaSparkContext jsc = new JavaSparkContext(sparkTrace.sparkContext());
+        Dataset<Row> tempTrace = MongoSpark.load(jsc).toDF()
+            .select(
+                    col("user.$id.oid").as("user"),
+                    col("hotspot.$id.oid").as("hotspot"),
+                    col("entryTime")
+            ).sort(
+                    col("user"),
+                    col("entryTime").asc()
+            );
+        Map<String, String> readOverrides = new HashMap<>();
+        readOverrides.put("collection", "user");
+        ReadConfig readConfig = ReadConfig.create(jsc).withOptions(readOverrides);
+        Dataset<Row> tempUser = MongoSpark.load(jsc, readConfig).toDF()
+                .select(
+                        col("_id.oid")
+                );
+
+        String id;
+        List<Row> route,
+                list = tempUser.collectAsList(),
+                currentRoute = new ArrayList<>(),
+                longestKnownRoute = new ArrayList<>();
+        Dataset<Row> set;
+        List<RowRecord> records = new ArrayList<>();
+
+        for(Row row : list){
+            id = row.get(0).toString();
+            set = tempTrace
+                    .select("hotspot")
+                    .where("user = '" + id + "'");
+            route = set.collectAsList();
+            longestKnownRoute.clear();
+            for(Row currRoute: route){
+                if(currentRoute.contains(currRoute)){
+                    if(currentRoute.size() > longestKnownRoute.size()){
+                        longestKnownRoute = new ArrayList<>(currentRoute);
+                    }
+                    currentRoute.clear();
+                } else {
+                    currentRoute.add(currRoute);
+                }
+            }
+            if(currentRoute.size() > longestKnownRoute.size()){
+                longestKnownRoute = new ArrayList<>(currentRoute);
+            }
+            records.add(new RowRecord(id, longestKnownRoute.size(), new ArrayList<>(longestKnownRoute)));
+        }
+        for (RowRecord record : records) {
+            System.out.println(record.toString());
+            System.out.println("longestRoute");
+        }
+        jsc.close();
+    }
+
+    static class RowRecord implements Serializable {
+        String userId;
+        Integer numOfTraces;
+        String traces;
+
+        public RowRecord(String userId, Integer numOfTraces, List<Row> traces) {
+            this.userId = userId;
+            this.numOfTraces = numOfTraces;
+            StringBuilder stringBuilder = new StringBuilder();
+            for(Row row : traces){
+                stringBuilder.append(row).append(";");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            this.traces = stringBuilder.toString();
+        }
+
+        @Override
+        public String toString() {
+            return "userId='" + userId + '\'' +
+                    ", numOfTraces=" + numOfTraces +
+                    ", traces='" + traces + '\'';
+        }
     }
 }
